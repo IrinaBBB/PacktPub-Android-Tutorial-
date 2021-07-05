ffa28e12d48fc828b3e315ed99ed5e06b07d7343
Irina <balkarovairina@gmail.com>
2021-07-05 11:12:57 +0200

    add: WebView and InternetPermission

--- app/src/main/java/ru/irinavb/myapplication/MainActivity.kt
+++ app/src/main/java/ru/irinavb/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.irinavb.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val webView = WebView(this)
+
+        // webView.settings.javaScriptEnabled = true
+        true.also { webView.settings.javaScriptEnabled = it }
+        setContentView(webView)
+        webView.loadUrl("https://www.google.com")
     }
 }
