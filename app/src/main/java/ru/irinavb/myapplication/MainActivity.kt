f234f6181b81f1b6c1ac6b07097312be2c2048d1
Irina <balkarovairina@gmail.com>
2021-07-22 12:28:30 +0200

    add: Example of Log Messages

--- app/src/main/java/ru/irinavb/myapplication/MainActivity.kt
+++ app/src/main/java/ru/irinavb/myapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.irinavb.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+        Log.v(TAG, "onCreate")
+        Log.i(TAG, "onCreate")
+        Log.w(TAG, "onCreate")
+        Log.e(TAG, "onCreate")
+        Log.wtf(TAG, "onCreate")
+    }
+
+    companion object {
+        private const val TAG = "Main Activity"
     }
 }
