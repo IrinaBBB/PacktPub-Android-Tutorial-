2daed48e9d929373ceadc33a9d9b66447ed1aded
Irina <balkarovairina@gmail.com>
2021-07-22 13:05:47 +0200

    add: creating layout values with get()

--- app/src/main/java/ru/irinavb/myapplication/MainActivity.kt
+++ app/src/main/java/ru/irinavb/myapplication/MainActivity.kt
@@ -2,10 +2,38 @@ package ru.irinavb.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val discountButton: Button
+        get() = findViewById(R.id.discount_button)
+
+    private val firstName: EditText
+        get() = findViewById(R.id.first_name)
+
+    private val lastName: EditText
+        get() = findViewById(R.id.last_name)
+
+    private val email: EditText
+        get() = findViewById(R.id.email)
+
+    private val discountCodeConfirmation: TextView
+        get() = findViewById(R.id.discount_code_confirmation)
+
+    private val discountCode: TextView
+        get() = findViewById(R.id.discount_code)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+    }
+
+    companion object {
+        private const val TAG = "Main Activity"
     }
 }
